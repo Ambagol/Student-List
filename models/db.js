ab40f44c1693d00f6f727a855d2ce23087b45258
Abdul Ghani <Ghani@Ganeshs-iMac.local>
2018-11-10 15:57:11 -0800

    added mongo path

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-mongoose.connect('mongodb://localhost:27017/StudentDB', { useNewUrlParser:true}, (err)=> {if(!err){console.log('MongoDb Connection Success')};
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser:true}, (err)=> {if(!err){console.log('MongoDb Connection Success')};
 });
 
 require('./student.model')
