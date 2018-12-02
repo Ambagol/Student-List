3d24eecacd47f46e37b90de34d21e8989af0019d
Ambagol Ganesh <Ambagol@Ganeshs-iMac.local>
2018-12-01 22:11:01 -0800

    updated mongo credentials

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 const mongoose =require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser:true}, (err)=> {if(!err){console.log('MongoDb Connection Success')};
+
+mongoose.connect("mongodb://ambagol:O@io4they@ds157923.mlab.com:57923/heroku_nt2fmg8m", { useNewUrlParser:true}, (err)=> {if(!err){console.log('MongoDb Connection Success')};
 });
 
 require('./student.model')
